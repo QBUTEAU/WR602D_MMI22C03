da18d6806a174c7de8ac03d2d1138eccf3100dc1
Github MMI3 <github@mmi-troyes.fr>
2025-03-05 14:04:09 +0000

    feat: phpcbf on github actions

--- src/Service/SymfonyDocs.php
+++ src/Service/SymfonyDocs.php
@@ -20,13 +20,10 @@ class SymfonyDocs
 
         $statusCode = $response->getStatusCode();
         // $statusCode = 200
-      
         $contentType = $response->getHeaders()['content-type'][0];
         // $contentType = 'application/json'
-        
         $content = $response->getContent();
         // $content = '{"id":521583, "name":"symfony-docs", ...}'
-        
         $content = $response->toArray();
         // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
 
